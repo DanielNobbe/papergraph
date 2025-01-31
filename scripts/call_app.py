1870a719831b0e2580513d11f3a74d0efbafb588
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 16:40:21 +0100

    updated test script

--- scripts/call_app.py
+++ scripts/call_app.py
@@ -2,12 +2,16 @@ import requests
 
 # Prepare the file to be uploaded
 
-path = 'data/2410.07959v1.pdf'
+path = 'data/2501.05409v2.pdf'
 
 files = {'file': open(path, 'rb')}
 
 # Send the POST request to the FastAPI server
-response = requests.post('http://localhost:8000/process/', files=files)
+# response = requests.post('http://localhost:8000/process/', files=files)
+
+response = requests.post('https://papergraph-47463167639.europe-central2.run.app/process', files=files)
+
+print(response.text)
 
 # Print the response from the server
 output_file = 'output.json'
