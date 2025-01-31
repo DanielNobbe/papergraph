0755270f4dd9cc9865e1c0b5171809f0e48ff1e2
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 16:40:55 +0100

    run docker with env var and local creds

--- sh/docker_run.sh
+++ sh/docker_run.sh
@@ -1,1 +1,2 @@
- docker run -p 8000:8000 -t papergraph:test
+ docker run -p 8000:8000 -e PORT=8000 -v ~/.config/gcloud/application_default_credentials.json:/var/secrets/google/key.json \
+  -e GOOGLE_APPLICATION_CREDENTIALS=/var/secrets/google/key.json -t papergraph:test 
