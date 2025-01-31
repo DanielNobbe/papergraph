319aa79441964de08ca15ea2ec692889ef96bee4
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 12:40:23 +0100

    set up linting etc

---
+++ sh/run_linting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Running Ruff..."
+poetry run ruff check .
+
+echo "Running Flake8..."
+poetry run flake8 .
+
+echo "Running Black..."
+poetry run black --check .
+
+echo "Running Pylint..."
+poetry run pylint .
+
+echo "Running MyPy..."
+poetry run mypy .
