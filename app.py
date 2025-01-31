109049b5df3596e3596819e40c228070a2298926
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 15:27:31 +0100

    move app.py to localhost

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ async def run_graph(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
