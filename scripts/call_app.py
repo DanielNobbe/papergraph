c174a6d38bfadedbfb881051e61b2bbe823b6191
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 18:01:09 +0100

    cleanup

--- scripts/call_app.py
+++ scripts/call_app.py
@@ -1,9 +1,9 @@
-# pylint: disable=C0103
+# pylint: disable=C0103,
+from argparse import ArgumentParser
 import requests
 
-from argparse import ArgumentParser
-
 # Prepare the file to be uploaded
+
 
 def main(args):
     path = args.file
@@ -14,8 +14,11 @@ def main(args):
 
         # Send the POST request to the FastAPI server
         if args.local:
-            response = requests.post(f'http://localhost:{args.port}/process/',
-                                     files=files)
+            response = requests.post(
+                f'http://localhost:{args.port}/process/',
+                files=files
+                timeout=600
+            )
         else:
             endpoint = args.url + "/process"
 
@@ -33,11 +36,31 @@ def main(args):
         with open(output_file, 'w', encoding='utf-8') as f:
             f.write(response.text)
 
+
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--local", type=bool, help="Include to call local")
-    parser.add_argument("--url", type=str, default="https://papergraph-47463167639.europe-central2.run.app", help="Set remote url.")
-    parser.add_argument("--port", type=int, default=8000, help="Set port for local mode.")
-    parser.add_argument("--file", type=str, default='data/2501.05409v2.pdf', help="Set file to process.")
-    args = parser.parse_args()
-    main(args)
+    parser.add_argument(
+        "--local",
+        type=bool,
+        help="Include to call local"
+    )
+    parser.add_argument(
+        "--url",
+        type=str,
+        default="https://papergraph-47463167639.europe-central2.run.app",
+        help="Set remote url."
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=8000,
+        help="Set port for local mode."
+    )
+    parser.add_argument(
+        "--file",
+        type=str,
+        default='data/2501.05409v2.pdf',
+        help="Set file to process."
+    )
+    arguments = parser.parse_args()
+    main(arguments)
