1ad3fa9cc76f8a418da5a5e631b191353ee6b60d
Daniel Nobbe <daniellnobbe@gmail.com>
2025-01-31 15:28:54 +0100

    small tweaks

--- scripts/call_app.py
+++ scripts/call_app.py
@@ -2,12 +2,12 @@ import requests
 
 # Prepare the file to be uploaded
 
-path = 'data/1910.09840v3.pdf'
+path = 'data/2410.07959v1.pdf'
 
 files = {'file': open(path, 'rb')}
 
 # Send the POST request to the FastAPI server
-response = requests.post('http://127.0.0.1:8000/process/', files=files)
+response = requests.post('http://localhost:8000/process/', files=files)
 
 # Print the response from the server
 output_file = 'output.json'
